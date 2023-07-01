8a178482940cff2a46e3a32c646047b9aafcc699
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-07-01 01:49:36 +0000

    [Save]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "registrations",
+    sessions: "sessions"
+  }
   root to: 'homes#top'
   get 'about' => 'homes#about'
-
+  resources :users, only: [:index, :show, :edit, :update]
 end
